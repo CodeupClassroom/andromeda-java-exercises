ca3fb2c26635ba14949c0e497c48cb542d270efd
Justin Reich <justinreich.dev@gmail.com>
2019-05-13 11:35:58 -0500

    add hello world exercise

---
+++ src/HelloWorld.java
@@ -0,0 +1,9 @@
+public class HelloWorld {
+
+    public static void main(String[] args) {
+        System.out.print("Hello");
+        System.out.print(" ");
+        System.out.print("World");
+    }
+
+}
